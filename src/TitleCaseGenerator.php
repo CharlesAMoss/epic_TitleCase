ce2e375b022b7c2252b71a89673509cc3cd31827
Joe Hartman & Charles Moss <student@epicodus.com>
2015-08-10 12:22:50 -0700

    add tests and logic to support edge cases

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
 
 class TitleCaseGenerator
 {
 	function makeTitleCase($input_title)
 	{
+		$input_title = strtolower($input_title);
+		$reserved_word = array("from", "and", "or", "the", "to", "on");
 	    $input_array_of_words = explode(" ", $input_title);
 	    $output_titlecased = array();
 	    foreach ($input_array_of_words as $word) {
-	        array_push($output_titlecased, ucfirst($word));
-	    }
+			if(in_array($word, $reserved_word)) {
+				array_push($output_titlecased, lcfirst($word));
+			} else {
+				array_push($output_titlecased, ucfirst($word));
+			}
+		}
+		$output_titlecased[0] = ucfirst($output_titlecased[0]);
+
 	    return implode(" ", $output_titlecased);
 	}
 }
--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -4,14 +4,27 @@
 
 	class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
 	{
+		function test_makeTitleCase_oneLetter()
+		{
+			//It capitalizes a single letter
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "a";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("A", $result);
+		}
 
 		function test_makeTitleCase_oneWord()
-		{
+		{	//It capitalizes the first letter of a single word
 			//Arrange
 			$test_TitleCaseGenerator = new TitleCaseGenerator;
 			$input = "deadbeef";
 
-			//Act 
+			//Act
 			$result = $test_TitleCaseGenerator->makeTitleCase($input);
 
 			//Assert
@@ -19,17 +32,95 @@
 		}
 
 		function test_makeTitleCase_multpleWords()
-		{
+		{   //It capitalizes a each the first letter in each word in a string
 			//Arrange
 			$test_TitleCaseGenerator = new TitleCaseGenerator;
 			$input = "homer simpson";
 
-			//Act 
+			//Act
 			$result = $test_TitleCaseGenerator->makeTitleCase($input);
 
 			//Assert
 			$this->assertEquals("Homer Simpson", $result);
 		}
-	}
 
+		function test_makeTitleCase_desigWords()
+		{	//It does not capitalize ariticles, conjuntions, and prepositions
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "beowulf from brighton beach";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("Beowulf from Brighton Beach", $result);
+		}
+
+		function test_makeTitleCase_firstDesigWords()
+		{	//It capitalizes ariticles, conjuntions, and prepositions only if they are the first word in a string.
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "from beowulf to the hulk";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("From Beowulf to the Hulk", $result);
+		}
+
+		function test_makeTitleCase_nonLetter()
+		{//It handles non-letter characters
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "57 beowulf endings!!";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("57 Beowulf Endings!!", $result);
+		}
+
+		function test_makeTitleCase_allCap()
+		{//It handles "ALL CAPS" strings and returns title case formate
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "BEOWULF ON THE ROCKS";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("Beowulf on the Rocks", $result);
+		}
+
+		function test_makeTitleCase_mixedCap()
+		{//It handles "MiXeD cApS" strings and returns title case format
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "BeoWulf aNd mE";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("Beowulf and Me", $result);
+		}
+
+		function test_makeTitleCase_uniqueStrings()
+		{//It handles unique strings like "O'Malley" or "McCool"
+			//Arrange
+			$test_TitleCaseGenerator = new TitleCaseGenerator;
+			$input = "here's to beowulf and McDuff and O'Malley";
+
+			//Act
+			$result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+			//Assert
+			$this->assertEquals("Here's to Beowulf and McDuff and O'Malley", $result);
+		}
+
+	}
 ?>
